dc50eb315294844144f8c729effb9c478bb495ec
Swastik517 <swastik05.banerjee@gmail.com>
2025-08-08 12:36:39 +0530

    Update k8s-deploy.sh with correct container name

--- k8s-deploy.sh
+++ k8s-deploy.sh
@@ -7,7 +7,7 @@ echo "Updating kubeconfig"
 aws eks update-kubeconfig --region us-east-1 --name legacy-cluster1
 
 echo "Updating deployment with new image: $IMAGE_TAG"
-kubectl set image deployment/energymicroservice energymicroservice=$REPOSITORY_URI:$IMAGE_TAG
+kubectl set image deployment/energymicroservice energylegacyapp=$REPOSITORY_URI:$IMAGE_TAG
 
 echo "Waiting for rollout to complete"
 kubectl rollout status deployment/energymicroservice
